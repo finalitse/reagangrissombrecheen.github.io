b9c4d25fe7e5bdc29a029d51e58f9d80429cdf75
reagan grissom brecheen <reagangrissom@gmail.com>
2018-12-04 22:37:00 -0600

    My first project

---
+++ Final/scripts/myFunctions.js
@@ -0,0 +1,10 @@
+/* Reagan Grissom Brecheen
+November 8, 2018
+My Function List
+*/
+
+//Thank you alert
+function tyAlert() {
+	alert("Thank you for your viewing my portfolio. I will contact you soon.");
+}
+
